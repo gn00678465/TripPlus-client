6137d328508def38b32d2d76b54f9020178d5ec8
Madao <gn00678465@gmail.com>
2023-04-23 19:32:10 +0800

    feat: 新增共用 Layout component 和設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import type { ReactElement, ReactNode } from 'react';
+import type { NextPage } from 'next';
 import { Noto_Sans_TC } from 'next/font/google';
 import localFont from 'next/font/local';
 import { Chakra } from '@/components';
@@ -32,12 +34,19 @@ const alkatra = localFont({
   variable: '--font-alkatra'
 });
 
-export default function App({ Component, pageProps }: AppProps) {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
   return (
     <main className={`${noto_sans_tc.variable} ${alkatra.variable} font-sans`}>
-      <Chakra>
-        <Component {...pageProps} />
-      </Chakra>
+      <Chakra>{getLayout(<Component {...pageProps} />)}</Chakra>
     </main>
   );
 }
