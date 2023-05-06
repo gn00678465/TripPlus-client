bb27a09c1fffef242654d1983d96c2517b2c191b
Madao <gn00678465@gmail.com>
2023-05-07 05:49:13 +0800

    fix: 修正 404 頁面錯誤

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import type { ReactElement } from 'react';
 import { Layout } from '@/components';
-import pageNotFound from '../assets/images/404.svg';
+import pageNotFound from '../assets/images/404_Error.png';
 import { Button } from '@chakra-ui/react';
 
 export async function getStaticProps() {
@@ -19,8 +19,16 @@ export default function Custom404() {
         <title>網頁不存在</title>
       </Head>
       <div className="flex h-full flex-col items-center justify-center gap-y-3 pt-2">
-        <Image src={pageNotFound} alt="網頁不存在"></Image>
-        <Link passHref href="/">
+        <Image
+          src={pageNotFound}
+          width={500}
+          height={500}
+          alt="網頁不存在"
+          priority
+          placeholder="blur"
+          blurDataURL="../assets/images/404_Error.png"
+        ></Image>
+        <Link passHref href="/" legacyBehavior>
           <Button as="a" colorScheme="primary">
             回首頁
           </Button>
