5137838d1d9bebc1d8c559402ee899a58f511079
Madao <gn00678465@gmail.com>
2023-05-06 20:12:04 +0800

    style: 調整顯示文字

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import type { ReactElement } from 'react';
 import { Layout } from '@/components';
 import pageNotFound from '../assets/images/404.svg';
-import { Center, Button } from '@chakra-ui/react';
+import { Button } from '@chakra-ui/react';
 
 export async function getStaticProps() {
   return {
@@ -16,10 +16,10 @@ export default function Custom404() {
   return (
     <>
       <Head>
-        <title>Page Not Found</title>
+        <title>網頁不存在</title>
       </Head>
       <div className="flex h-full flex-col items-center justify-center gap-y-3 pt-2">
-        <Image src={pageNotFound} alt="Page Not Found"></Image>
+        <Image src={pageNotFound} alt="網頁不存在"></Image>
         <Link passHref href="/">
           <Button as="a" colorScheme="primary">
             回首頁
