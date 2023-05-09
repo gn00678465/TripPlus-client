c3fd14d0ff1019044c4bca18388a40c9ce3d8922
Madao <gn00678465@gmail.com>
2023-05-09 14:40:48 +0800

    refactor: refactor useAuthStore

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,11 +1,60 @@
 import { create } from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
 
-type AuthState = {
-  isLogin: boolean;
-  setIsLogin: (isLogin: boolean) => void;
+// type AuthState = {
+//   isLogin: boolean;
+//   setIsLogin: (isLogin: boolean) => void;
+// };
+
+// export const useAuthStore = create<AuthState>((set) => ({
+//   isLogin: false,
+//   setIsLogin: (isLogin) => set({ isLogin })
+// }));
+
+type State = {
+  userInfo: ApiAuth.UserInfo | null;
+  _hasHydrated: boolean;
+  getters: {
+    isLogin: boolean;
+  };
 };
 
-export const useAuthStore = create<AuthState>((set) => ({
-  isLogin: false,
-  setIsLogin: (isLogin) => set({ isLogin })
-}));
+type Actions = {
+  setUserInfo: (arg: ApiAuth.UserInfo) => void;
+  setHasHydrated: (state: boolean) => void;
+};
+
+export const useAuthStore = create<State & Actions>()(
+  devtools(
+    persist(
+      (set, get) => ({
+        userInfo: null,
+        _hasHydrated: false,
+        setUserInfo: (params) => {
+          set((state) => ({ ...state, userInfo: params }));
+        },
+        setHasHydrated: (state) => {
+          set({
+            _hasHydrated: state
+          });
+        },
+        getters: {
+          get isLogin() {
+            return !!get().userInfo;
+          }
+        }
+      }),
+      {
+        name: 'userInfo',
+        partialize: (state) =>
+          Object.fromEntries(
+            Object.entries(state).filter(([key]) => ['userInfo'].includes(key))
+          ),
+        onRehydrateStorage: () => (state) => {
+          state && state.setHasHydrated(true);
+        }
+      }
+    ),
+    { name: 'authStore' }
+  )
+);
