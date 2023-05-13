e5f3b9645cd35c6db06889e02545eacd485eee3b
Madao <gn00678465@gmail.com>
2023-05-14 06:48:54 +0800

    types: 調整 omit 型別

--- src/utils/object/omit.ts
+++ src/utils/object/omit.ts
@@ -3,5 +3,5 @@ export function omit<T extends object>(data: T, keys: (keyof T)[]) {
     Object.entries(data).filter(
       ([key, value]) => !keys.includes(key as keyof T)
     )
-  );
+  ) as Omit<T, (typeof keys)[number]>;
 }
