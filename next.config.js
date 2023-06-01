9a40f6498e9dfed08382646d5e707d5736d00f3d
Madao <gn00678465@gmail.com>
2023-06-01 09:16:00 +0800

    feat: 調整 next.config.js & service utils

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     BASE_API_URL: process.env.BASE_API_URL
   },
   images: {
-    domains: ['res.cloudinary.com', 'picsum.photos'],
+    domains: ['res.cloudinary.com', 'picsum.photos', 'images.unsplash.com'],
     remotePatterns: [
       {
         protocol: 'https',
