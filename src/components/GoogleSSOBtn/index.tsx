84b5e33a0108f5dacf92add150a10b10edb5aa7e
Madao <gn00678465@gmail.com>
2023-06-08 09:02:57 +0800

    feat: 拆出 Google 按鈕

---
+++ src/components/GoogleSSOBtn/index.tsx
@@ -0,0 +1,20 @@
+import NextLink from 'next/link';
+import { Button, Icon } from '@chakra-ui/react';
+import { FcGoogle } from 'react-icons/fc';
+
+export default function GoogleSSOBtn() {
+  function handleGoogleSSO() {
+    const w = window.open(
+      'http://localhost:3005/auth/google',
+      'mozillaWindow',
+      'popup,width=500,height=700'
+    );
+  }
+
+  return (
+    <Button colorScheme="gray" width={'100%'} my={4} onClick={handleGoogleSSO}>
+      <Icon as={FcGoogle} mr={1} className="text-xl" />
+      使用 Google 帳號登入
+    </Button>
+  );
+}
